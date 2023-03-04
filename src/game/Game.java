b0fd049c383f097bb24e509eaa77a51259b4168d
Abdelrahmane Bekhli <120100975+Abdelrahmane-Bekhli@users.noreply.github.com>
2023-03-04 18:04:35 +0000

    Focus request
    Setting up the focus so that the game is focused when clicked on the window

--- src/game/Game.java
+++ src/game/Game.java
@@ -7,26 +7,34 @@ import javax.swing.plaf.basic.BasicTreeUI;
 
 public class Game{
     public Game(){
+        //creating the world
         GameWorld world = new GameWorld();
-
+        //creating the world view
         GameView view = new GameView(world, 1000, 562);
 
+        //controlling the character
         CharacterController controller = new CharacterController(world.getCharacter());
         view.addKeyListener(controller);
 
+        //naming the game
         final JFrame frame = new JFrame("2D platformer");
         frame.add(view);
 
+        //set the X button to terminante the program
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationByPlatform(true);
-
         frame.setResizable((false));
 
         frame.pack();
 
+        //display the game
         frame.setVisible(true);
 
         world.start();
+
+        //give focus to the game when clicked on the window
+        GiveFocus focus = new GiveFocus(view);
+        view.addMouseListener(focus);
 
         view.requestFocus();
     }
