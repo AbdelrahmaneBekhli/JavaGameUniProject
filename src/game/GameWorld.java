d35b8b49f61cd6005184a5467754c398e080e8f2
Abdelrahmane Bekhli <abdelrahmane001@gmail.com>
2023-03-05 20:54:50 +0000

    Level platforms added

--- src/game/GameWorld.java
+++ src/game/GameWorld.java
@@ -2,6 +2,9 @@ package game;
 
 import city.cs.engine.*;
 import org.jbox2d.common.Vec2;
+
+import javax.swing.*;
+
 public class GameWorld  extends World{
     private Character character;
 
@@ -9,17 +12,34 @@ public class GameWorld  extends World{
         super();
 
         //ground platform
-        Shape shape = new BoxShape(15, 0);
+        Shape shape = new BoxShape(25, 0.5f);
         StaticBody ground = new StaticBody(this, shape);
-        ground.setPosition(new Vec2(-10f, -8.5f));
+        ground.setPosition(new Vec2(-0f, -13.5f));
 
         //platform
-        Shape platformShap = new BoxShape(3, 0.5f);
-        StaticBody platform1 = new StaticBody(this, platformShap);
-        platform1.setAngle(40);
+        Shape ShortplatformShape = new BoxShape(3, 0.5f);
+        Shape MediumPlatformShape = new BoxShape(6, 0.5f);
 
-        StaticBody platform2 = new StaticBody(this, platformShap);
-        platform2.setPosition(new Vec2(- 4, -5f));
+        StaticBody platform1 = new StaticBody(this, ShortplatformShape);
+        platform1.setPosition(new Vec2(-7, -7));
+
+        StaticBody platform2 = new StaticBody(this, ShortplatformShape);
+        platform2.setPosition(new Vec2(4, -7f));
+
+        StaticBody platform3 = new StaticBody(this, MediumPlatformShape);
+        platform3.setPosition(new Vec2(19, -2f));
+
+        StaticBody platform4 = new StaticBody(this, MediumPlatformShape);
+        platform4.setPosition(new Vec2(0, 0f));
+
+        StaticBody platform5 = new StaticBody(this, MediumPlatformShape);
+        platform5.setPosition(new Vec2(-20, -3f));
+
+        StaticBody platform6 = new StaticBody(this, ShortplatformShape);
+        platform6.setPosition(new Vec2(10, 5f));
+
+        StaticBody platform7 = new StaticBody(this, MediumPlatformShape);
+        platform7.setPosition(new Vec2(-20, 5f));
 
         //character
 
