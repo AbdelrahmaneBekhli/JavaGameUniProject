d8414f32d6f527d96eb5f809e679f6c19a5b22ab
Abdelrahmane Bekhli <120100975+Abdelrahmane-Bekhli@users.noreply.github.com>
2023-03-10 16:53:48 +0000

    Credits
    Added credits system to the game, as well as making the character bounce upon on killing an enemy

--- src/game/GenericCollisionListener.java
+++ src/game/GenericCollisionListener.java
@@ -2,8 +2,13 @@ package game;
 
 import city.cs.engine.CollisionEvent;
 import city.cs.engine.CollisionListener;
+import org.jbox2d.common.Vec2;
 
 public class GenericCollisionListener implements CollisionListener {
+    private final Character character;
+    public GenericCollisionListener(Character character) {
+        this.character = character;
+    }
 
     @Override
     public void collide(CollisionEvent collisionEvent) {
@@ -12,6 +17,7 @@ public class GenericCollisionListener implements CollisionListener {
             //check if the character collides with the slime from the top
             if (collisionEvent.getReportingBody().getPosition().y > collisionEvent.getOtherBody().getPosition().y + 1.5f){
                 collisionEvent.getOtherBody().destroy();
+                character.setBounce(true);
             } else {
                 collisionEvent.getReportingBody().destroy();
             }
@@ -19,6 +25,8 @@ public class GenericCollisionListener implements CollisionListener {
         //check if the collision is with a coin
         if(collisionEvent.getOtherBody() instanceof Coin){
             collisionEvent.getOtherBody().destroy();
+            character.incrementcredits();
+            System.out.println(character.getCredits());
         }
     }
 
