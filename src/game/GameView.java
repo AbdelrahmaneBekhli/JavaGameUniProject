4bf93c76fa508d713c689340485b508f32f5399a
Abdelrahmane Bekhli <abdelrahmane001@gmail.com>
2023-03-10 16:54:51 +0000

    Display coins

--- src/game/GameView.java
+++ src/game/GameView.java
@@ -7,16 +7,29 @@ import javax.swing.*;
 import java.awt.*;
 public class GameView extends UserView{
 
-    private Image background;
-    public GameView(GameWorld world, int width, int height) {
+    private final Image background;
+    private final Character character;
+    public GameView(GameWorld world,Character character, int width, int height) {
 
         super(world, width, height);
         background = new ImageIcon("data/background.jpg").getImage();
+        this.character = character;
 
     }
 
     @Override
     protected void paintBackground(Graphics2D image){
         image.drawImage(background, 0, 0, this);
+    }
+
+    @Override
+    protected void paintForeground(Graphics2D image){
+        Font font = new Font("Arial", Font.BOLD, 30);
+        image.setFont(font);
+        image.setColor(Color.BLACK);
+
+        //Coins information
+        String coins = "Coins: " + character.getCredits();
+        image.drawString(coins, 0,30);
     }
 }
