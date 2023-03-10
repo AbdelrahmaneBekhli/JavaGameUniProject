39042067a8078153559817073f37b60aa81aa090
Abdelrahmane Bekhli <abdelrahmane001@gmail.com>
2023-03-10 18:41:58 +0000

    Platform class

---
+++ src/game/Platform.java
@@ -0,0 +1,24 @@
+package game;
+
+import city.cs.engine.*;
+import org.jbox2d.common.Vec2;
+
+public class Platform extends StaticBody {
+    public Platform(World world, Shape shape, float x, float y, String type) {
+        super(world, shape);
+        this.setPosition(new Vec2(x, y));
+        this.setClipped(true);
+        if(type.equals("short")) {
+            BodyImage shortPlatformImage = new BodyImage("data/ShortPlatform.png", 1f);
+            this.addImage(shortPlatformImage);
+        }
+        else if(type.equals("medium")){
+            BodyImage mediumPlatformImage = new BodyImage("data/MediumPlatform.png", 1f);
+            this.addImage(mediumPlatformImage);
+        }
+        else{
+            BodyImage GroundImage = new BodyImage("data/GroundPlatform.png", 1f);
+            this.addImage(GroundImage);
+        }
+    }
+}
