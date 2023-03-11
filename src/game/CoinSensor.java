e0785f980070d61687d387b3b7963b202b5f9132
Abdelrahmane Bekhli <120100975+Abdelrahmane-Bekhli@users.noreply.github.com>
2023-03-11 21:02:16 +0000

    Update CoinSensor.java

--- src/game/CoinSensor.java
+++ src/game/CoinSensor.java
@@ -2,11 +2,12 @@ package game;
 
 import city.cs.engine.Body;
 import city.cs.engine.Shape;
+import city.cs.engine.Sensor;
 
-public class CoinSensor extends city.cs.engine.Sensor {
+public class CoinSensor extends Sensor {
     public CoinSensor(Body body, Shape shape, Coin coin, Character character) {
         super(body, shape);
         CoinSensorListener sensorListener = new CoinSensorListener(character, coin);
         this.addSensorListener(sensorListener);
     }
-}
+}dwd
