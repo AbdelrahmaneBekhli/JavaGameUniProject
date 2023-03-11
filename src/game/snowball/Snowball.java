2af9e391d945f6a2c2ee403172c78233600b9448
Abdelrahmane Bekhli <120100975+Abdelrahmane-Bekhli@users.noreply.github.com>
2023-03-11 22:18:19 +0000

    Create Snowball.java

---
+++ src/game/snowball/Snowball.java
@@ -0,0 +1,17 @@
+package game.snowball;
+
+import city.cs.engine.BodyImage;
+import city.cs.engine.DynamicBody;
+import city.cs.engine.Shape;
+import city.cs.engine.World;
+
+public class Snowball extends DynamicBody {
+    public Snowball(World world, Shape shape) {
+        super(world, shape);
+        SnowCollisionListener SnowCollisions = new SnowCollisionListener();
+        this.addCollisionListener(SnowCollisions);
+
+        BodyImage snowballImage = new BodyImage("data/snowball.png", 0.7f);
+        this.addImage(snowballImage);
+    }
+}
