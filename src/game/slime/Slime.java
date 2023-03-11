00bb50996213bb26146cf315834124df19918961
Abdelrahmane Bekhli <120100975+Abdelrahmane-Bekhli@users.noreply.github.com>
2023-03-11 22:18:53 +0000

    Made slime ghost fixture

---
+++ src/game/slime/Slime.java
@@ -0,0 +1,105 @@
+package game.slime;
+
+import city.cs.engine.*;
+import game.character.Character;
+import game.slime.sensor.SlimeSensor;
+
+import javax.swing.*;
+import java.awt.desktop.AboutEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Slime extends Walker implements StepListener, ActionListener {
+    private static final Shape SlimeShape = new BoxShape(1,0.75f);
+    private static final BodyImage rightImage = new BodyImage("data/enemy/slime_right.gif", 1.5f);
+    private static final BodyImage leftImage = new BodyImage("data/enemy/slime_left.gif", 1.5f);
+
+    private float left;
+    private float right;
+    private final float range;
+    private boolean alive = true;
+    private String facing;
+
+    private boolean bounce = true;
+    public Slime(World world, float range, String initial_facing, Character character) {
+        super(world);
+        world.addStepListener(this);
+        this.range = range;
+        this.setGravityScale(0);
+        Fixture fixture = new GhostlyFixture(this, SlimeShape);
+        Sensor sensor = new SlimeSensor(this, SlimeShape, character);
+
+        if (initial_facing.equals("right")){
+            this.addImage(rightImage);
+            this.startWalking(3);
+            this.facing = "right";
+        } else {
+            this.addImage(leftImage);
+            this.startWalking(-3);
+            this.facing = "left";
+        }
+    }
+
+    public void setRange(){
+        //setting up the range for each direction
+        this.left = this.getPosition().x - range;
+        this.right = this.getPosition().x + range;
+    }
+
+    @Override
+    public void preStep(StepEvent stepEvent) {
+        if(this.alive) {
+            //changing speed and gif when the range is reached
+            if (getPosition().x > right) {
+                this.removeAllImages();
+                this.addImage(leftImage);
+                this.startWalking(-3);
+                this.facing = "left";
+            }
+            if (getPosition().x < left) {
+                this.removeAllImages();
+                this.addImage(rightImage);
+                this.startWalking(3);
+                this.facing = "right";
+            }
+        }
+        else{
+            this.startWalking(0);
+        }
+    }
+
+    public void die(){
+        this.bounce = false;
+        this.alive = false;
+        if (this.facing.equals("right")) {
+            BodyImage image = new BodyImage("data/enemy/die_right.gif", 1.5f);
+            this.removeAllImages();
+            this.addImage(image);
+        } else{
+            BodyImage image = new BodyImage("data/enemy/die_left.gif", 1.5f);
+            this.removeAllImages();
+            this.addImage(image);
+        }
+        Timer timer = new Timer(1000, new SlimeTimerHandler(this));
+        timer.start();
+
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public boolean isBounce() {
+        return bounce;
+    }
+
+    @Override
+    public void postStep(StepEvent stepEvent) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
