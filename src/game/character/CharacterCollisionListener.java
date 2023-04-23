7bee3a80263e2c03976f2529eade547bfd3ec37c
Abdelrahmane Bekhli <abdelrahmane001@gmail.com>
2023-04-23 17:09:37 +0100

    created jump pad

--- src/game/character/CharacterCollisionListener.java
+++ src/game/character/CharacterCollisionListener.java
@@ -2,13 +2,22 @@ package game.character;
 
 import city.cs.engine.CollisionEvent;
 import city.cs.engine.CollisionListener;
+import game.JumpPad;
 import game.enemies.Spike;
+import org.jbox2d.common.Vec2;
 
 public class CharacterCollisionListener implements CollisionListener {
     @Override
     public void collide(CollisionEvent collisionEvent) {
         if(collisionEvent.getOtherBody() instanceof Spike){
             ((Character) collisionEvent.getReportingBody()).die();
+        }
+        if(collisionEvent.getOtherBody() instanceof JumpPad){
+            Character character = (Character) collisionEvent.getReportingBody();
+            JumpPad jumpPad = (JumpPad) collisionEvent.getOtherBody();
+            if(((int) character.getPosition().y >= ((int) jumpPad.getPosition().y + 2))){
+                character.applyImpulse(new Vec2(0,700));
+            }
         }
     }
 }
