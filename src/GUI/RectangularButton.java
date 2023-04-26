ef6a851148712dd69e75a0dc47d870ea0c58741b
Abdelrahmane Bekhli <abdelrahmane001@gmail.com>
2023-04-26 18:04:57 +0100

    created rectangular Jbutton

---
+++ src/GUI/RectangularButton.java
@@ -0,0 +1,19 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RectangularButton extends JButton {
+    public RectangularButton(int xPos, int yPos, int width, int height, String image, String hoverImage) {
+        Image originalImage = new ImageIcon(image).getImage();
+        Image hoverOverImage = new ImageIcon(hoverImage).getImage();
+        //button properties
+        this.setBounds(xPos, yPos, width, height);
+        this.setBorderPainted(false);
+        this.setContentAreaFilled(false);
+        this.setFocusPainted(false);
+        //adding the images on the button
+        this.setIcon(new ImageIcon(originalImage));
+        this.setRolloverIcon(new ImageIcon(hoverOverImage));
+    }
+}
