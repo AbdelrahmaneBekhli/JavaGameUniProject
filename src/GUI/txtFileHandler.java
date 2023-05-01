c8e9bb912accb7d1ceac0fdd79c12734d504c327
Abdelrahmane Bekhli <120100975+Abdelrahmane-Bekhli@users.noreply.github.com>
2023-05-01 15:11:48 +0100

    return leaderboard as an ArrayList

--- src/GUI/txtFileHandler.java
+++ src/GUI/txtFileHandler.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class txtFileHandler {
     private final String fileName;
@@ -18,7 +19,7 @@ public class txtFileHandler {
         FileWriter writer = null;
         try {
             writer = new FileWriter(fileName, append);
-            writer.write(name + "," + score + "\n");
+            writer.write("\n" + name + "," + score);
         } finally {
             if (writer != null) {
                 writer.close();
@@ -26,23 +27,30 @@ public class txtFileHandler {
         }
     }
 
-    public void readScores() throws IOException {
+    public ArrayList<Object[]> readScores() throws IOException {
         FileReader fr = null;
         BufferedReader reader = null;
+        ArrayList<Object[]> leaderboardList = new ArrayList<>();
         try {
-            System.out.println("Reading " + fileName + " ...");
             fr = new FileReader(fileName);
             reader = new BufferedReader(fr);
             String line = reader.readLine();
+            Object[] playerScore;
             while (line != null) {
-                // file is assumed to contain one name, score pair per line
-                String[] tokens = line.split(",");
-                String name = tokens[0];
-                int score = Integer.parseInt(tokens[1]);
-                System.out.println("Name: " + name + ", Score: " + score);
-                line = reader.readLine();
+                if(line.equals("")){
+                    System.out.println("empty");
+                    line = reader.readLine();
+                }else {
+                    // file is assumed to contain one name, score pair per line
+                    String[] tokens = line.split(",");
+                    String name = tokens[0];
+                    int score = Integer.parseInt(tokens[1]);
+                    playerScore = new Object[]{name, score};
+                    line = reader.readLine();
+                    //adding data to the array list
+                    leaderboardList.add(playerScore);
+                }
             }
-            System.out.println("...done.");
         }  finally {
             if (reader != null) {
                 reader.close();
@@ -51,5 +59,6 @@ public class txtFileHandler {
                 fr.close();
             }
         }
+        return leaderboardList;
     }
 }
